28841e13e66b72f33461da737def4f10c30a5938
Diego <diegogode.bonani@gmail.com>
2025-05-25 20:51:23 -0300

    .

--- backend/myapp/admin.py
+++ backend/myapp/admin.py
@@ -8,7 +8,15 @@ from .models import(
 
 ) 
 class PedidoAdmin(admin.ModelAdmin):
-    list_display = ('cliente', 'entregador', 'observacoes')
+    list_display = ('cliente', 'entregador', 'descricao', 'observacoes', 'pago', 'data_pedido')
+    list_filter = ('pago', 'entregador', 'data_pedido')
+    ordering = ('-data_pedido',)
+    fieldsets = (
+    ('Informações Básicas', {'fields': ('cliente', 'descricao', 'observacoes', 'pago',)}),
+    ('Detalhes do Entregador', {'fields': ('entregador',)}),
+  )
+
+
 
 
 admin.site.register(Pedido, PedidoAdmin)
